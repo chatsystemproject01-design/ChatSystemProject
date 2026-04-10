5a0a998748b49a38c0af2dd992a3b81ea00d868e
Hồng Duy Điền <duydien3504@gmail.com>
2026-04-10 21:09:49 +0700

    Add Lib

--- test_create_all.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.extensions import db
-from app.models import *
-
-app = create_app()
-with app.app_context():
-    print("Creating all tables via db.create_all()...")
-    db.create_all()
-    print("Tables created successfully.")
